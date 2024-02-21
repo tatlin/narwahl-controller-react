e95592761896950abe29037d9397f899486c8252
matt jezyk <matt.jezyk@ambr.net>
2024-02-21 08:08:47 -0500

    setup basic material ui and react-three-fiber web app

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
+import { Box } from '@react-three/drei';
+import { Container, AppBar, Toolbar, Typography } from '@mui/material';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6">React Three Fiber with Material-UI</Typography>
+        </Toolbar>
+      </AppBar>
+      <Container>
+        <Typography variant="body1" gutterBottom>
+          This is a basic example of integrating React Three Fiber with Material-UI in a TypeScript React application.
+        </Typography>
+        <Canvas>
+          <ambientLight />
+          <pointLight position={[10, 10, 10]} />
+          <OrbitControls />
+          <Box position={[-1.2, 0, 0]} />
+        </Canvas>
+      </Container>
     </div>
   );
 }
